24faa3bf78955eddb1e1e5246bf78782417edddc
iana-coder <carafachiara0@gmail.com>
2025-09-18 16:02:03 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "majella-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./assets/compass.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Installazione: salva i file nella cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Attivazione: pulizia vecchie cache se necessario
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercetta richieste e serve dalla cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
